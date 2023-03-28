d9b2411413b3461e53dbc3ff6225162fe53cda38
Vinicius Martins <viniciusmartinsga@gmail.com>
2023-03-28 13:47:07 -0300

    Deleting an useless file

---
+++ binary_hexadecimal_octal_conversion.py
@@ -0,0 +1,10 @@
+
+number = input()
+
+in_base = int(input())
+
+out_base = int(input())
+
+x = len(int(number))
+
+y = x/
---
+++ conversor_decimal.py
@@ -0,0 +1,12 @@
+
+in_number = input("Number: ")
+
+
+
+in_digits = []
+
+for
+
+
+
+print(in_digits)
