432a6fdc856a662089d690864d74743250d06140
Vinicius Martins <viniciusmartinsga@gmail.com>
2023-03-29 13:52:10 -0300

    puts all the code snippets together in a single code

--- Number_System_Conversion.py
+++ Number_System_Conversion.py
@@ -2,212 +2,13 @@
 
 number_o = input("Number to be converted: \n")
 
-
+number_d = 0
 
 base_o = int(input("Original  base: \n"))
 
 base_d = int(input("Conversion base: \n"))
 
-
-#CONVERSION FROM DECIMAL TO BINARY
-
-if base_o == 10 and base_d == 2:
-
-    integer = int(number_o)
-
-    fraction = float(number_o) - integer
-
-    #INTEGER PART CONVERSION
-
-    binary = []
-
-    while integer > 0:
-
-        binary.append(integer%2)
-
-        integer = int((integer - integer%2) / 2)
-
-    binary = binary[::-1]
-
-    mystring = ''
-
-    for bit in binary:
-        bitstring = str(bit)
-        mystring += '' + bitstring
-
-    #FRACTIONAL PART CONVERSION
-
-    binary_fraction = []
-
-    while True:
-
-        fraction = fraction * 2
-
-        binary_fraction.append(int(fraction))
-
-        if fraction == 0:
-            break
-
-        fraction = fraction - int(fraction)
-
-    mystring_fraction = ''
-
-    for bit in binary_fraction:
-        bitstring = str(bit)
-        mystring_fraction += '' + bitstring
-
-    #PRINT RESULTS
-
-    print(f"{number_o} em decimal é igual a {mystring}.{mystring_fraction} em binário")
-
-#CONVERSION FROM DECIMAL TO OCTAL
-
-elif base_o == 10 and base_d == 8:
-
-    integer = int(number_o)
-
-    fraction = float(number_o) - integer
-
-    #INTEGER PART CONVERSION
-
-    octal = []
-
-    while integer > 0:
-
-        octal.append(int(integer%8))
-
-        integer = (integer - integer%8) / 8
-
-    octal = octal[::-1]
-
-    octal_str = ''
-
-    for oct_digit in octal:
-        oct_digit_str = str(oct_digit)
-        octal_str += '' + oct_digit_str
-
-    #FRCATIONAL PART CONVERTION
-
-    octal_f = []
-
-    while True:
-
-        fraction = fraction * 8
-
-        octal_f.append(int(fraction))
-
-        fraction = fraction - int(fraction)
-
-        if fraction == 0 :
-            break
-
-    octal_f_str = ''
-
-    for octal_f_digit in octal_f:
-        octal_f_digit_str = str(octal_f_digit)
-        octal_f_str += '' + octal_f_digit_str
-
-
-    print(f"{number_o} em decimal é igual a {octal_str}.{octal_f_str} em octal")
-
-#CONVERSION FROM DECIMAL TO HEXADECIMAL
-
-elif base_o == 10 and base_d == 16:
-
-    integer = int(number_o)
-
-    fraction = float(number_o) - integer
-
-    #INTEGER CONVERSION
-
-    hexa = []
-
-    while integer > 0:
-
-        hexa.append(int(integer%16))
-
-        integer = (integer - integer%16)/16
-
-    hexa = hexa[::-1]
-
-
-    for a in range(len(hexa)):
-
-        if hexa[a] == 10:
-            hexa[a] = 'A'
-
-        elif hexa[a] == 11:
-            hexa[a] = 'B'
-
-        elif hexa[a] == 12:
-            hexa[a] = 'C'
-
-        elif hexa[a] == 13:
-            hexa[a] = 'D'
-
-
-        elif hexa[a] == 14:
-            hexa[a] = 'E'
-
-        elif hexa[a] == 15:
-            hexa[a] = 'F'
-
-    hexa_str = ''
-
-    for hexadigit in hexa:
-        hexadigit_str = str(hexadigit)
-        hexa_str += '' + hexadigit_str
-
-    #FRACTIONAL CONVERTION
-
-    hexa_f = []
-
-    while True:
-
-        fraction = fraction * 16
-
-        hexa_f.append(int(fraction))
-
-        if fraction == 0:
-            break
-
-        fraction = fraction - int(fraction)
-
-    for a in range(len(hexa_f)):
-
-        if hexa_f[a] == 10:
-            hexa_f[a] = 'A'
-
-        elif hexa_f[a] == 11:
-            hexa_f[a] = 'B'
-
-        elif hexa_f[a] == 12:
-            hexa_f[a] = 'C'
-
-        elif hexa_f[a] == 13:
-            hexa_f[a] = 'D'
-
-
-        elif hexa_f[a] == 14:
-            hexa_f[a] = 'E'
-
-        elif hexa_f[a] == 15:
-            hexa_f[a] = 'F'
-
-    hexa_f_str = ''
-
-    for hexa_f_digit in hexa_f:
-        hexa_f_digit_str = str(hexa_f_digit)
-        hexa_f_str += '' + hexa_f_digit_str
-
-    print(f"{number_o} em decimal é igual a {hexa_str}.{hexa_f_str} em hexadecimal")
-
-#CONVERSION FROM OTHER NUMBER SYSTEMS TO DECIMAL
-
-elif base_o != 10 and base_d == 10:
-
-
-    digit_list = [str(x) for x in number_o]
+def hexa():
 
     if base_o == 16:
 
@@ -232,9 +33,48 @@ elif base_o != 10 and base_d == 10:
             elif digit_list[d] == 'F':
                 digit_list[d] = '15'
 
-    digit_list = digit_list[::-1]
 
-    number_d = 0
+def naming(base):
+
+    global base_name
+
+    if base == 2:
+        base_name = "binário"
+
+    elif base == 8:
+        base_name = "octal"
+
+    elif base == 16:
+        base_name = "hexadecimal"
+
+
+if base_d == base_o:
+    print("\nAs bases são iguais")
+
+#CONVERSION FROM DECIMAL TO OTHER NUMBER SYSTEMS
+
+elif base_o == 10:
+
+    digit_list = [int(x) for x in str(number_o)]
+
+    for p in range(len(digit_list)):
+
+        number_d = number_d + digit_list[p]*base_o**p
+
+    naming(base_d)
+
+    print(f"\n{number_o} em decimal é igual a {number_d} em {base_name}")
+
+
+#CONVERSION FROM OTHER NUMBER SYSTEMS TO DECIMAL
+
+elif base_o != 10 and base_d == 10:
+
+    digit_list = [str(x) for x in number_o]
+
+    hexa()
+
+    digit_list = digit_list[::-1]
 
     for p in range(len(digit_list)):
 
@@ -242,4 +82,61 @@ elif base_o != 10 and base_d == 10:
 
         number_d = number_d + digit_list[p] * base_o**p
 
-    print (f"{number_d}")
+
+    naming(base_o)
+
+    print (f"\n{number_o} em {base_name} é igual a {number_d} em decimal")
+
+
+#CONVERSION BETWEEN BINARY, OCTAL AND HEXADECIMAL
+
+elif base_o != 10 and base_d != 10:
+
+    digit_list = [str(x) for x in str(number_o)]
+
+    hexa()
+
+    digit_list = digit_list [::-1]
+
+    #conversion to decimal
+
+    decimal = 0
+
+    for p in range(len(digit_list)):
+
+        digit_list[p] = int(digit_list[p])
+
+        decimal = decimal + digit_list[p]*base_o**p
+
+
+    #conversion from decimal to the destination base
+
+    digit_list_d = []
+
+    while decimal > 0:
+
+        digit_list_d.append(decimal%base_d)
+
+        decimal = int((decimal - decimal%base_d) / base_d)
+
+
+    digit_list_d = digit_list_d[::-1]
+
+    number_d = ''
+
+    for digit in digit_list_d:
+        digitstring = str(digit)
+        number_d += '' + digitstring
+
+    
+    naming(base_o)
+
+    print (f"{number_o} em {base_name}  é igual a:")
+
+    naming(base_d)
+           
+    print(f"{number_d} em {base_name}")
+
+
+else:
+    print("\nInput inválido. Apenas conversões entre decima, hexadecimal, binário e octal")
